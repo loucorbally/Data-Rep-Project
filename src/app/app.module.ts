07994c79fc19e7f14e7db7fa6c11b86ad92a1942
loucorbally <louisecorbally1@gmail.com>
2018-11-07 17:27:01 +0000

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoCreateComponent } from './to-do-create/to-do-create.component';
+import { ToDoDetailsComponent } from './to-do-details/to-do-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoCreateComponent,
+    ToDoDetailsComponent
   ],
   imports: [
     BrowserModule,
